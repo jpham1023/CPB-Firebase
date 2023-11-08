0b53af0f4b34feda244fd8f73d7fc8806faf5b4d
Jhhs xcode <phamthaoquyen@gmail.com>
2023-11-08 10:49:59 -0600

    Initial Commit

---
+++ CPB-Firebase/CPB_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPB_FirebaseApp.swift
+//  CPB-Firebase
+//
+//  Created by Jenny Pham on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CPB_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
